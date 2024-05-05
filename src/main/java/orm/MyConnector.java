13259e8675233b7aef6ca2a7a2969df01cf662ad
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-05-05 22:27:50 +0300

    05052024

---
+++ src/main/java/orm/MyConnector.java
@@ -0,0 +1,23 @@
+package orm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MyConnector {
+    private static Connection connection;
+    private static final String connectionString = "jdbc:mysql://192.168.5.200:3306/custom-orm";
+
+    public static void createConnection(String username, String password, String dbName) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", username);
+        properties.setProperty("password", password);
+
+        connection = DriverManager.getConnection(connectionString, properties);
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
