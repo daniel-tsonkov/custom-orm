70a1c1d8870d7e7ffe115f95ef4f087325781002
dan <daniel_tzonkov@abv.bg>
2024-05-07 09:31:30 +0300

    07052024

--- src/main/java/orm/MyConnector.java
+++ src/main/java/orm/MyConnector.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class MyConnector {
     private static Connection connection;
     //private static final String connectionString = "jdbc:mysql://192.168.5.200:3306/"; //local
-    private static final String connectionString = "jdbc:mysql://78.90.227.248:3306/"; //external
+    private static final String connectionString = "jdbc:mysql://78.90.227.249:3306/"; //external
 
     //private MyConnector() {}
 
