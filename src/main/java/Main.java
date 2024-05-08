d0cc4658c5d831a874a51591b5c1dea8bc85f358
dan <daniel_tzonkov@abv.bg>
2024-05-08 14:34:30 +0300

    08052024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,8 +19,9 @@ public class Main {
         EntityManager<User> userEntityManager = new EntityManager<>(connection);
 
         User user = new User("Pesho", 25, LocalDate.now());
-        user.setId(2);
-        user.setUsername("pesho_new_final");
+        //user.setId(2);
+        //user.setUsername("pesho_new_final");
+        user.setUsername("pesho_new_finalv2");
 
         //userEntityManager.doCreate(User.class);
         //userEntityManager.doAlter(User.class);
@@ -30,7 +31,9 @@ public class Main {
         //System.out.println(first);
         //Iterable<User> first = userEntityManager.find(User.class, "id < 5");
         //System.out.println(first.toString());
-        Iterable<User> first = userEntityManager.find(User.class);
-        System.out.println(first.toString());
+        //Iterable<User> first = userEntityManager.find(User.class);
+        //System.out.println(first.toString());
+
+        userEntityManager.delete(user);
     }
 }
